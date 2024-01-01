5a1c236f9b96098ce71183303523fde4562ebe78
TH-montassar <montassar.themri@gmail.com>
2024-01-01 22:21:15 +0100

    fixe header

--- src/Components/StartPage.jsx
+++ src/Components/StartPage.jsx
@@ -4,22 +4,19 @@ import { Button } from "@material-tailwind/react";
 import LogoSagemcom from "../Assets/Logo/logo-sagemcom-new-charte-header.png";
 
 export const StartPage = () => {
-//ToDo
-/*   const [isLinkActive, setIsLinkActive] = useState(false);
+  const handleNavigation = () => {
+    localStorage.clear(); // Clear local storage before navigation
+  };
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsLinkActive(true);
-    }, 2000);
-
-    return () => clearTimeout(timeout);
-  }, []);
-*/
   return (
     <React.Fragment>
       <div className="flex flex-col items-center gap-16 m-auto w-screen  ">
         <img src={LogoSagemcom} alt="Logo" />
-        <Link to="/home/PermanentParameters/ppGet" className="my-auto">
+        <Link
+          to="/home/PermanentParameters/ppGet"
+          onClick={handleNavigation}
+          className="my-auto"
+        >
           <Button
             size="lg"
             variant="outlined"
